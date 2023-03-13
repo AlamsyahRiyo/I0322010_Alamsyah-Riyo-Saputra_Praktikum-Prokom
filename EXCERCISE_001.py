f1708cddaf1629730b65bfc256ae52c46e045dad
AlamsyahRiyo <riyoalamsyah@student.uns.ac.id>
2023-03-13 14:44:58 +0700

    Tugas Prokom 1

---
+++ EXCERCISE_001.py
@@ -0,0 +1,25 @@
+print(  "Alamsyah Riyo Saputra" )
+print(  "I0322010" )
+
+
+
+
+
+
+print("Tugas Praktikum Program Komputer".center(50,"="))
+
+
+
+
+
+
+
+print(      "Saya besar dan tumbuh disuatu kota kecil bernama Kota Magelang, kota dimana jumlah penduduknya taklebih dari 2 juta ini menyimpan banyak kenangan masa kecil hingga masa remaja saya."
+      "Magelang merupakan salah satu kota ternyaman  yang pernah saya tinggali selama ini. Udara yang sejuk serta keramahan warganya merupakan alasan orang tua saya menetap dan istirahat selamanya di kota ini."
+      "Saya dibesarkan di lingkungan sekitar yang sangat baik, kota yang memiliki tingkat kriminalitas yang sangat rendah sehingga Magelang banyak dijadikan orang tempat peristirahatan ketika masa tua."
+      "Dari masa kanak kanak hingga remaja banyak nilai nilai kehidupan yang saya dapat di kota ini sehingga tak heran banyak kenangan , suka duka yang saya alami sangat membekas di benak saya."
+      "Komplek saya tinggal mempunyai lingkungan yang asli didukung dengan fasilitas yang sangat menunjang semua hobi saya seperti jogging, tennis lapangan, dan renang. Sehingga anak seumuran saya"
+      "bisa menikmati masa remajanya dengan baik. Pemandangan Gunung Sumbing dikala pagi dan sejuknya udara yang tidak bisa saya dapatkan di Solo. Ketika sore tiba saya sering mengunjungi padang sabana hijau"
+      "serta melihat sapi sapi menyantap rumput segar. Tak terasa saya sudah memasuki masa kuliah dimana masa ini merupakan batu lompatan saya untuk meraih kesuksesan, masa masa dimana saya menentukan arah dan tujuan saya hidup."
+      "Di fakultas teknik terutama jurusan yang saya ambil yaitu teknik industri menempa saya agar menjadi pribadi yang kritis dalam menghadapi suatu masalah dan bertanggung jawab. Dunia perkuliahan memang sangat kompleks." 
+      "Sehingga saya berharap bisa mendapatkan pengalaman serta belajar banyak selama duduk di bangku perkuliahan")
